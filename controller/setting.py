e4dc5c184a199c3e66ad847dc090464a023da9c0
Khang DT <khangdt.work@gmail.com>
2023-10-29 08:53:28 +0700

    Add code converting local database of plot setting to class object - Setting.py

--- controller/setting.py
+++ controller/setting.py
@@ -1,52 +1,111 @@
 import os, glob
 import pandas as pd
 from typing import List
+from typing import TypeAlias
 from controller.workstatus import Status
 
+"""
+A Figure contains multiple subplot object.
+Each subplot object is a single box plot which are object returned by matplotlib.pyplot.boxplot()
+"""
 
-class SinglePlot:
+
+
+
+class Subplot:
+
+    def __init__(
+        self, 
+        subplotname: str = "",
+        lowerspec: float = -1,
+        upperspec: float = -1,
+        to_plot: bool = False,
+        figurename: str = "" ):
+
+        self.subplotname = subplotname
+        self.lowerspec = lowerspec
+        self.upperspec = upperspec
+        self.to_plot = to_plot
+        self.figurename  = figurename
+
+
+class Figure:
 
     def __init__(self):
 
-        self.plotname = ""
-        self.lowerspec = -1
-        self.upperspec = -1
-        self.is_plot = False
-        self.figureContainerName = ""
-
-
-class MultiPlotFigure:
-
-    def __init__(self):
-
-        self.plotFigureList: List[SinglePlot] = []
-        self.containerFigureName = ""
+        self.subplot_list: List[Subplot] = []
+        self.name = ""
         self.rowsize = 0
         self.columnsize = 0
+
 
 class Setting:
 
     rootdir = os.path.abspath('')
     file_ext = 'csv'
     local_plot_list_database = 'setting_plot.csv'
-    input_dirname = "Input"
-    output_dirname = "Output"
-    reading_cols = None
     
+    input_dir = os.path.abspath("Input")
+    output_dir = os.path.abspath("Output")
+    
+    reading_cols = None
     data_row_to_skipread = [1, 2]
 
+    plotpages = List[Figure]
     # def load_local_plot_setting_database
 
 
     def __init__(self):
         self.plot_list_database = pd.DataFrame()
-        self.input_dir = os.path.abspath(self.input_dirname)
-        self.output_dir = os.path.abspath(self.output_dirname)
-
         self.plotPages = List[MultiPlotFigure]
         
 
-    # def 
+    def _dataframe_to_plotpages(self, dataframe: pd.DataFrame) ->[]:
+
+        try:
+            if list(dataframe.columns) != ['Plot Item', 'LSL', 'USL', 'To Plot', 'Figure']:
+                Status.error_message("Plot item in database have incorrect header.")
+                Status.setting_update_ok = False
+                return
+        except:
+            return
+
+        # Divide dataframe to groups by column ['Figure']
+        datagroups = dataframe.groupby(['Figure'], sort=False)
+        
+        # Iterate through each Figure group in dataframe and convert to PlotPage class object
+        for groupname, group_data in datagroups:
+            
+            figure = Figure()
+            figure.name = groupname
+            
+            for index, row in group_data:
+                subplot = Subplot(
+                    subplotname = row['Plot Item'],
+                    lsl = row['LSL'],
+                    usl = row['USL'],
+                    to_plot = row['To Plot'],
+                    figurename = row['Figure'])
+
+                figure.plot_list.append(plot)
+
+            self.plotpages.append(figure)
+
+        return self.plotpages
+
+            
+            
+            
+            
+            
+
+
+        
+
+
+
+    
+
 
 
 
@@ -55,20 +114,15 @@ class Setting:
 
         self.plot_list_database = pd.read_csv(self.local_plot_list_database, index_col=None, sep=',', header=0)
         
-        # Validating the Plot List data loaded from local database (CVS file)
-        # Need to implement printing the Invalid case to Main log UI
-        if list(self.plot_list_database.columns) != ['Plot Item', 'LSL', 'USL', 'To Plot', 'Figure']:
-            Status.error_message("Plot item in database have incorrect header.")
-            Status.setting_update_ok = False
-            return
-        else:
-            Status.setting_update_ok = True
         
         # load Plot list in local file database to object
         # update status of input data
         Status.list_import_files = [file for file in glob.glob(self.input_dir + './*.{}'.format(self.file_ext))]
 
+        # Update the plot item in local database of setting to class object
+        Setting.plotpages = self._dataframe_to_plotpages(self.plot_list_database)
 
+        print("debug here")
 
 
 
