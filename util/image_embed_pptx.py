7b5866b1c32d7ea119e4f0016e880ed73ac3814b
lgdvh\khangdt <khangdt.work@gmail.com>
2024-02-23 18:34:46 +0700

    New sync

--- util/image_embed_pptx.py
+++ util/image_embed_pptx.py
@@ -10,7 +10,6 @@ class ImageEmbedPptx:
     PPTX_FILENAME = 'Template_Report.pptx'
     LIST_IMAGE_FILEPATHS: List[str] = Setting.LIST_FIGURE_IMAGES
 
-
     def export_figures_to_pptx(self) -> None:
         # check file existence
         if not os.path.exists(self.PPTX_FILENAME):
@@ -22,18 +21,23 @@ class ImageEmbedPptx:
             print(f'Failed to open Template file {self.PPTX_FILENAME} for exporting image to.')
             return
 
-        # Remove the old figures
-        # Add the new figures
+        # Push images to PPT file
+        slide_layout = ppt_output.slide_layouts[0]
+        max_slides = len(self.ppt_output.slides)
         i = 0
         for img in self.LIST_IMAGE_FILEPATHS:
             current_slide = ppt_output.slides[i]
             plot_img = current_slide.shapes.add_picture(img, Inches(0.40), Inches(4.85), width=Inches(5.30))
-            
+
             # Send the figures to the back
             ref_element = current_slide.shapes[0]._element
             ref_element.addprevious(plot_img._element)
 
             # Go to next slide in PPTX file
+            if i >= (max_slides - 1):
+                max_slides += 1
+                # add one more slide
+
             i += 1
 
         # Save the PPT
